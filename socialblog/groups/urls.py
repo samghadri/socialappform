9f2c666454705a260452cb279a0d268eb88ef613
samghadri <sam.ghadri@gmail.com>
2017-11-11 23:33:39 +0000

    seventh Commmit

---
+++ socialblog/groups/urls.py
@@ -0,0 +1,11 @@
+from django.confurls import url
+from . import views
+
+app_name = 'groups'
+
+urlpatterns =[
+    url(r'^$',views.ListGroups.as_view(), name = 'all'),
+    url(r'^create/$',views.CreateGroup.as_view(),name='create'),
+    url(r'posts/in/(?P/<slug>[-\w]+)/$',views.SingleGroup.as_view(),name ='single'),
+
+]
